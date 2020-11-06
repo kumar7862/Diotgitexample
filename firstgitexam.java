abfa7ca93990fac694caf125a4ca933522d7c104
Amit Bairwa <sampkumar78672@gmail.com>
2020-11-06 11:56:18 +0530

    Added 2 functions comitted

--- firstgitexam.java
+++ firstgitexam.java
@@ -2,5 +2,10 @@ class firstgitexam{
     public static void main(String[] args)
     {
         System.out.println("Hello world!!!!");
+        System.out.println("Calling even numbers!!!!");
+        SecondExample.printEvenNumbers(11);
+        
+
+
     }
 }
