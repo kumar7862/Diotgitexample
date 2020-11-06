afde069080b6e2d34b8832bf4c5f30227b57fdd7
Amit Bairwa <sampkumar78672@gmail.com>
2020-11-06 10:59:12 +0530

    multiple files comitted

---
+++ SecondExample.java
@@ -0,0 +1,8 @@
+class SecondExample{
+    public static void printEvenNumbers(int no)
+    {
+        System.out.println("Even numbers upto" +no);
+        for(int x=0; x<no; x=x+2)
+            System.out.printlnI(x);
+    }
+}
